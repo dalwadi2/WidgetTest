704b3e3bdbf85d6199561dc83e2a8abe486d06bd
Harsh Dalwadi <dalwadi2@gmail.com>
2016-09-07 10:29:17 +0530

    Initial

--- app/src/main/java/in/vaksys/widgettest/new11/Constants.java
+++ app/src/main/java/in/vaksys/widgettest/new11/Constants.java
@@ -3,12 +3,12 @@ package in.vaksys.widgettest.new11;
 
 public class Constants {
 
-	public static final String PACKAGE_NAME = "com.ecs.android.sample.widget";
-	public static final String TAG = "AndroidWidgetSample";
-	public static final String INTENT_EXTRA_WIDGET_TEXT = "INTENT_EXTRA_WIDGET TEXT";
-	public static final String ACTION_WIDGET_UPDATE_FROM_ACTIVITY = "ACTION_WIDGET_UPDATE_FROM_ACTIVITY";
-	public static final String ACTION_WIDGET_UPDATE_FROM_ALARM = "ACTION_WIDGET_UPDATE_FROM_ALARM";
-	public static final String ACTION_WIDGET_UPDATE_FROM_WIDGET = "ACTION_WIDGET_UPDATE_FROM_WIDGET";
-	public static final String ALARM_STATUS = "ALARM_STATUS";
+    public static final String PACKAGE_NAME = "com.ecs.android.sample.widget";
+    public static final String TAG = "AndroidWidgetSample";
+    public static final String INTENT_EXTRA_WIDGET_TEXT = "INTENT_EXTRA_WIDGET TEXT";
+    public static final String ACTION_WIDGET_UPDATE_FROM_ACTIVITY = "ACTION_WIDGET_UPDATE_FROM_ACTIVITY";
+    public static final String ACTION_WIDGET_UPDATE_FROM_ALARM = "ACTION_WIDGET_UPDATE_FROM_ALARM";
+    public static final String ACTION_WIDGET_UPDATE_FROM_WIDGET = "ACTION_WIDGET_UPDATE_FROM_WIDGET";
+    public static final String ALARM_STATUS = "ALARM_STATUS";
 
 }
